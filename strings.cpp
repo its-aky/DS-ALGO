3d39d189c31e3e540ff04ec889510136442d997a
its-aky <kumaramityadav230@gmail.com>
2024-03-08 18:54:54 +0530

    Created strings.cpp

---
+++ strings.cpp
@@ -0,0 +1,3 @@
+//important problems
+//https://www.geeksforgeeks.org/problems/check-if-strings-are-rotations-of-each-other-or-not-1587115620/1
+//https://www.geeksforgeeks.org/problems/longest-palindrome-in-a-string/0
