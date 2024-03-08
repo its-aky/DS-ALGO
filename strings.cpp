6d7f69ff32029801a2685d2ce3e0001d7f7f706d
its-aky <kumaramityadav230@gmail.com>
2024-03-08 21:25:10 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -1,3 +1,12 @@
 //important problems
+
+//Logic:Concatenate the string1 with itself and check whether string2 is substring of concatenated string 
 //https://www.geeksforgeeks.org/problems/check-if-strings-are-rotations-of-each-other-or-not-1587115620/1
+
+
+//Checking maximum length of palindromic substring in a given string and also generating the string as output 
+//Logic:for i(0->n-1) generate all substring using j(i->n-1) and pass this i and j to helper which tell that substring starting at i and ending at j is palindromic or not
+//maintain a start and lenght of maximum palindrome possible to generate the substring
 //https://www.geeksforgeeks.org/problems/longest-palindrome-in-a-string/0
+
+
